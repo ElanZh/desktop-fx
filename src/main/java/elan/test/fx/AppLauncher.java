c2713ce04e6c2d125f0385de473a040d98c2c4a7
ElanZh <elan_zh@163.com>
2020-01-29 20:38:34 +0800

    修正：maven打包后无法启动

---
+++ src/main/java/elan/test/fx/AppLauncher.java
@@ -0,0 +1,13 @@
+package elan.test.fx;
+
+import javafx.application.Application;
+
+/**
+ * @author 张一然
+ * @date 2020/1/29 下午8:36
+ */
+public class AppLauncher {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
